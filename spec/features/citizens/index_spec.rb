b130f566c2a3a36d8208861d35970590db060312
a.mukharryamov <a.mukharryamov@fun-box.ru>
2022-03-22 15:20:51 +0300

    citizens#index feature test

---
+++ spec/features/citizens/index_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+feature 'get collection of citizens', %q(
+  In order to see a list of citizens
+  I'd like to be able to request it
+) do
+  given!(:citizens) { create_list(:citizen, 2) }
+
+  scenario 'user visit root page' do
+    visit citizens_path
+
+    citizens.each do |citizen|
+      expect(page).to have_content citizen.name
+    end
+  end
+end
